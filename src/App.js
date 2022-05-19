71bdb36b0af13da7b8eaa54575618647e78015f3
Pedro Pinheiro <pedro.pinheiro000@hotmail.com>
2022-05-19 19:23:56 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,85 +2,139 @@ import './App.css';
 
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-
+import TopBarProgress from "react-topbar-progress-indicator";
 
 import { useState, useEffect } from 'react';
 
 
-const getPokeURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`
+const getPokeURL = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+const maxPoke = 898;
+
+TopBarProgress.config({
+  barColors: {
+    "0": "#f00",
+    "0.5": "#0f0",
+    "1.0": "#00f",
+  },
+  shadowBlur: 0
+});
 
 
 function App() {
-  const pokemonPromisses = []
+  var pokemonPromisses = [];
+
+  const [loadingItems, setLoadingItems] = useState(true);
   const [pokemonList, setPokemonList] = useState(undefined)
   const [selectedPage, setSelectedPage] = useState(1)
-  const [itemsPerPage, setItemsPerPage] = useState(20)
+  const itemsPerPage = 40
+  const paginateItems = Math.ceil(maxPoke / itemsPerPage);
+
+  const pageMaxItems = (itemsPerPage * (selectedPage - 1)) + itemsPerPage;
+  const maxPokeValue = pageMaxItems > maxPoke ? maxPoke : pageMaxItems;
+
 
   useEffect(() => {
-    for (let i = (itemsPerPage * (selectedPage - 1)) + 1; i <= (itemsPerPage * (selectedPage - 1)) + itemsPerPage; i++) {
+    for (let i = (itemsPerPage * (selectedPage - 1)) + 1; i <= maxPokeValue; i++) {
       pokemonPromisses.push(fetch(getPokeURL(i)).then(response => response.json()))
     }
 
     Promise.all(pokemonPromisses)
       .then(pokemons => {
         setPokemonList(pokemons)
+        setLoadingItems(false)
       })
   }, [selectedPage])
 
+  const SelectPage = (page) => {
+    if (page <= paginateItems && page >= 1) {
+      setLoadingItems(true)
+      setSelectedPage(page);
+    }
+  }
   return (
     <div className="App">
-
+       {loadingItems && <TopBarProgress />}
       <header className='search'>
-
         <img src='./pokemon.svg' alt=''></img>
-        <div style={{ display: 'flex', gap: '8px' }}>
-          <button onClick={() => { setSelectedPage(selectedPage - 1) }}>&#x3c;</button>
-          <div>{selectedPage}</div>
-          <button onClick={() => { setSelectedPage(selectedPage + 1) }}>&#x3e;</button>
-        </div>
-        <div>
-          <input></input>
-          <button>Pesquisar</button>
-        </div>
+        <Pagination
+          paginateSize={9}
+          selectedPage={selectedPage}
+          SelectPage={SelectPage}
+          paginateItems={paginateItems}
+        />
       </header>
-      {/* <Pagination /> */}
-      <RenderSelectedPage pkList={pokemonList} />
 
-
-
+      <RenderSelectedPage pkList={pokemonList} loadingDone={loadingItems} />
     </div>
   );
 }
 
+function Pagination({ paginateSize, selectedPage, SelectPage, paginateItems }) {
+  var tmp = [], leftRange;
+
+  if (selectedPage < Math.ceil(paginateSize / 2)) {
+    leftRange = 1;
+  } else if (selectedPage >= paginateItems - Math.floor(paginateSize / 2)) {
+    leftRange = paginateItems - paginateSize + 1;
+  } else {
+    leftRange = selectedPage - Math.ceil(paginateSize / 2) + 1;
+  }
+
+  for (var i = leftRange; i < (leftRange + paginateSize); i++) {
+    tmp.push(i);
+  }
+
+  return (
+    <div style={{ display: 'flex', gap: '8px', justifyContent: 'center', alignItems: 'center' }}>
+      <div className='pg_button' onClick={() => {
+        SelectPage(selectedPage - 1);
+      }}>🡸
+      </div>
+
+      {
+        tmp.map((item, index) => (
+          <div
+            className={`paginationItem ${(selectedPage === (item)) ? 'paginationItemActive' : ''}`}
+            key={index} style={{ cursor: 'pointer' }}
+            onClick={() => SelectPage(item)}>
+            {item}
+          </div>
+        ))
+      }
+
+
+      <div className='pg_button' onClick={() => {
+        SelectPage(selectedPage + 1);
+      }}>🡺
+      </div>
+    </div>
+  )
+}
+
 function RenderSelectedPage({ pkList }) {
   return (
-    <ul className='poketable'>
-      {!pkList ?
-        <div>Carregando...</div> :
-        pkList.map((data, index) => (
-          <div className='item' key={data.id}>
-            <div className='pokeID'>{data.id}</div>
-            <div className='pokeName'>{data.name}</div>
-            <div className='pokeIMG'>
-              <LazyLoadImage
-                alt={data.name}
-                height={140}
-                width={140} 
-                effect="blur"
-                src={data.sprites.other['official-artwork'].front_default}   
-                />
-              {/* <img alt='' src={data.sprites.other['official-artwork'].front_default}></img> */}
-
-
-            </div>
-            <div className='typeList'>
-              <PokeTypes pokeTypes={data} />
-            </div>
+    <div className='poketable'>
+      {pkList && pkList.map((data, index) => (
+        <div className='item' key={data.id}>
+          <div className='pokeID'>{data.id}</div>
+          <div className='pokeName'>{data.name}</div>
+          <div className='pokeIMG'>
+            <LazyLoadImage
+              alt={data.name}
+              height={140}
+              width={140}
+              effect="blur"
+              src={data.sprites.other['official-artwork'].front_default}
+            />
           </div>
-        )
-        )
+          <div className='typeList'>
+            <PokeTypes pokeTypes={data} />
+          </div>
+        </div>
+      )
+      )
       }
-    </ul>
+    </div>
   )
 }
 
